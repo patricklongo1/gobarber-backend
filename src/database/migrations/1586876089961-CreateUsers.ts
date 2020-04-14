f5ebeabef6cd9ce15d7640683cc0d78a906d0d34
patricklongo1 <ck.longo@bol.com.br>
2020-04-14 12:42:02 -0300

    migrations update

--- src/database/migrations/1586876089961-CreateUsers.ts
+++ src/database/migrations/1586876089961-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1586876089961 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
