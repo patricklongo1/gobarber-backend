026ff7b9607a920bf44222d96192572779c28f57
patricklongo1 <ck.longo@bol.com.br>
2020-04-14 17:35:45 -0300

    file upload/update and server file route

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import multerConfig from '../config/upload';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 const usersRouter = Router();
+const upload = multer(multerConfig);
 
 usersRouter.post('/', async (req, res) => {
   try {
@@ -23,5 +28,27 @@ usersRouter.post('/', async (req, res) => {
     return res.status(400).json({ success: false, message: error.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (req, res) => {
+    try {
+      const updateUserAvatar = new UpdateUserAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: req.user.id,
+        avatarFilename: req.file.filename,
+      });
+
+      delete user.password;
+
+      return res.json(user);
+    } catch (error) {
+      return res.status(400).json({ success: false, message: error.message });
+    }
+  },
+);
 
 export default usersRouter;
--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import 'reflect-metadata';
 
 import express from 'express';
 import routes from './routes';
+import multerConfig from './config/upload';
 
 import './database';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(multerConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
